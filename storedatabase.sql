f702731845b2267d93786d01ab679f0be838786b
Axel Eschholz <axelion737@gmail.com>
2022-04-11 14:14:02 -0300

    Updtated sql

--- storedatabase.sql
+++ storedatabase.sql
@@ -13,23 +13,10 @@ CREATE TABLE `category` (
 );
 
 CREATE TABLE `user` (
-  `user_id` INT UNSIGNED  AUTO_INCREMENT,
+  `user_id` INT UNSIGNED AUTO_INCREMENT,
   `password` VARCHAR(30),
   `name` VARCHAR(50),
   PRIMARY KEY (`user_id`)
-);
-
-CREATE TABLE `shopper` (
-  `shopper_id` INT UNSIGNED  AUTO_INCREMENT,
-  `user_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`shopper_id`),
-  FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`)
-);
-
-CREATE TABLE `list_content` (
-  `list_id` INT UNSIGNED,
-  `offering_id` INT UNSIGNED,
-  PRIMARY KEY (`list_id`, `offering_id`)
 );
 
 CREATE TABLE `product` (
@@ -53,10 +40,9 @@ CREATE TABLE `store` (
 );
 
 CREATE TABLE `manager` (
-  `manager_id` INT UNSIGNED AUTO_INCREMENT,
   `user_id` INT UNSIGNED NOT NULL,
   `store_id` INT UNSIGNED,
-  PRIMARY KEY (`manager_id`),
+  PRIMARY KEY (`user_id`, `store_id`),
   FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`),
   FOREIGN KEY (`store_id`) REFERENCES `store`(`store_id`)
 );
@@ -73,10 +59,19 @@ CREATE TABLE `offering` (
 
 CREATE TABLE `shopping_list` (
   `list_id` INT UNSIGNED AUTO_INCREMENT,
-  `shopper_id` INT UNSIGNED NOT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
   `name` VARCHAR(30),
   PRIMARY KEY (`list_id`),
-  FOREIGN KEY (`shopper_id`) REFERENCES `shopper`(`shopper_id`)
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`)
+);
+
+CREATE TABLE `list_content` (
+  `list_id` INT UNSIGNED,
+  `offering_id` INT UNSIGNED,
+  `quantity` INT UNSIGNED,
+  PRIMARY KEY (`list_id`, `offering_id`),
+  FOREIGN KEY (`list_id`) REFERENCES `shopping_list`(`list_id`),
+  FOREIGN KEY (`offering_id`) REFERENCES `offering`(`offering_id`)
 );
 
 
@@ -84,10 +79,7 @@ INSERT INTO `user` (`password`, `name`) VALUES
   ('1234', 'Lauriane Howe'),
   ('abcd', 'Jared Ruecker');
 
-INSERT INTO `shopper` (`user_id`) VALUES
-  (1);
-
-INSERT INTO `shopping_list` (`shopper_id`, `name`) VALUES
+INSERT INTO `shopping_list` (`user_id`, `name`) VALUES
   (1, 'Holiday Dinner List');
 
 INSERT INTO `category` (`name`) VALUES
@@ -100,7 +92,8 @@ INSERT INTO `category` (`name`) VALUES
 
 INSERT INTO `store` (`name`, `location`) VALUES
   ('Superstore', 'New Minas, NS'),
-  ('Sobeys', 'Wolfville, NS');
+  ('Sobeys', 'Wolfville, NS'),
+  ('Independent', 'Wolfville, NS');
   
   
 INSERT INTO `manager` (`user_id`, `store_id`) VALUES
@@ -109,11 +102,12 @@ INSERT INTO `manager` (`user_id`, `store_id`) VALUES
 INSERT INTO `product` (`category_id`, `name`, `brand`, `description`, `perishable`, `lifespan`, `organic`) VALUES
   (1, '3.25% Milk', 'Scotsburn', 'Really darn good milk for anything.', 'Yes', 30, 'No'),
   (3, 'Cabbage', 'Local', 'Fresh local cabbage, suitable for eating.', 'Yes', 60, 'Yes'),
-  (2, 'Captin Crunch', 'Kellogs', 'They\'Arrrrre great!', 'No', NULL, NULL),
-  (6, 'Canned Milk', 'Canning Co.', 'Not really sure why it\'s in a can.', 'No', NULL, NULL),
+  (2, 'Captin Crunch', 'Kellogs', 'TheyArrrrre great!', 'No', NULL, NULL),
+  (6, 'Canned Milk', 'Canning Co.', 'Not really sure why its in a can.', 'No', NULL, NULL),
   (5, 'In-Store Baked Bread', 'Local', 'Very fluffy and high in sugar.', 'Yes', 5, 'No'),
   (4, 'Cumin', 'Spice Co.', 'Pronounced que-min, not cum-in.', 'No', NULL, NULL),
-  (3, 'Oranges', '(tropical country name)', 'The orange variety. Peel is toxic (pesticides).', 'Yes', 20, 'No');
+  (3, 'Oranges', '(tropical country name)', 'The orange variety. Peel is toxic (pesticides).', 'Yes', 20, 'No'),
+  (3, 'Carrots', 'Not Local', 'Freshish no-local carrots, not suitable for eating.', 'Yes', 60, 'No');
 
 INSERT INTO `offering` (`store_id`, `product_id`, `price`) VALUES
   (2, 2, 4.6),
@@ -123,10 +117,12 @@ INSERT INTO `offering` (`store_id`, `product_id`, `price`) VALUES
   (2, 4, 3.5),
   (2, 5, 3.29),
   (1, 5, 2.19),
-  (1, 2, 2.4);
+  (1, 2, 2.4),
+  (3, 7, 6.99),
+  (3, 8, 2.99);
 
-INSERT INTO `list_content` (`list_id`, `offering_id`) VALUES
-  (1, 8),
-  (1, 4),
-  (1, 7),
-  (1, 5);
+INSERT INTO `list_content` (`list_id`, `offering_id`, `quantity`) VALUES
+  (1, 8, 3),
+  (1, 4, 1),
+  (1, 7, 2),
+  (1, 5, 1);
